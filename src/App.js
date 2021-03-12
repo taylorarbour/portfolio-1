e5896d17e846e3dcc49f9ddec7bfd2c7becfb4c9
taylorarbour <77947257+taylorarbour@users.noreply.github.com>
2021-03-12 00:37:47 -0500

    port

--- src/App.js
+++ src/App.js
@@ -1,70 +1,53 @@
 import "./App.css"
 import Menu from "./components/Menu"
+
 function App() {
   let data = [
     {
-      menuName: "Dinner",
+      menuName: "Taylor Arbour",
       menuItems: [
         {
           itemId: 1,
-          itemPrice: "12",
-          itemName: "Lasagne",
-          itemDescription:
-            "Meat and cheese layered between house-made pasta with bell peppers and onions.",
+          itemName: "Business Card",
+          imgFile: ('images/image1.png'),
+          itemDescription: "For a business card, click the button below!",
+            
         },
         {
           itemId: 2,
-          itemPrice: "10",
-          itemName: "Cheese Ravioli",
-          itemDescription: "Cheese-filled ravioli served with house red sauce.",
+          itemName: "Advanced Art",
+          imgFile: 'images/sunflower.jpg',
+          itemDescription: "This is a sunflower I made for a client, very detailed and time-consuming. I can most likely can design what you request!",
         },
         {
           itemId: 3,
-          itemPrice: "14",
-          itemName: "Chicken Parmesan",
+          itemName: "Profile Slide",
           itemDescription:
-            "Breaded chicken topped with marinara sauce and lots of cheese served over house made spaghetti.",
+            "A short and accurate PowerPoint slide that best describes you!",
         },
-      ],
-    },
-    {
-      menuName: "Dessert",
-      menuItems: [
         {
           itemId: 45,
-          itemPrice: "10",
-          itemName: "Chocolate Lava Cake",
-          itemDescription: "Dark chocolate molten lava cake. Serves 2-3.",
+          itemName: "Personal Logo",
+          itemDescription: "Need a personal logo for your photos, assignments, or just for fun? Request below!",
         },
         {
-          itemId: 47,
-          itemPrice: "8",
-          itemName: "Tiramisu",
-          itemDescription:
-            "Layers of espresso-soaked ladyfingers and marscarpone cream topped with cocoa and espresso powder.",
-        },
-        {
-          itemId: 49,
-          itemPrice: "5",
-          itemName: "Cannolis",
-          itemDescription:
-            "Three flaky pastries enclosing a rich, ricotta filling dotted with decadent chocolate chips.",
-        },
-        {
-          itemId: 50,
-          itemPrice: "5",
-          itemName: "Cappuccino",
-          itemDescription: "Steamed milk with two ristretto shots of espresso.",
+          itemId: 45,
+          itemName: "Personalized Front Covers",
+          itemDescription: "This is a Star Wars movie cover, I most likely can design what you request!",
         },
       ],
     },
+    
   ]
   return (
     <div className="App">
+      
       {data.map((menu) => (
         <Menu {...menu}></Menu>
       ))}
     </div>
+    
   )
+  
 }
 export default App
--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,20 +1,31 @@
 import React from "react"
 
 export default class MenuItem extends React.Component {
-    render() {
+     
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bool: false
+        }
+    }
+    changetoTrue(){
+        this.setState({bool:true});
+    }
+    render()
+        {
         return (
            <div>
                <span>{this.props.itemPrice}</span>
             <h2>{this.props.itemName}</h2>
             <p>{this.props.itemDescription}</p>
-            <button>Add To Cart</button>
-
-
-
-
-
-            </div>
-        );
-        
+            <img src={this.props.imgFile} alt=""/>
+            {
+                !this.state.bool
+                ? <button onClick={() => this.changetoTrue()}>Request a Quote</button>
+                : <p>Request Sent!</p>
+            }
+          
+            </div>);
+        }
     }
-}
